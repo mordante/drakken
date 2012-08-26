87c9c8873cbc64c954b3bc1c0d102bb887f248df
Mark de Wever <zar-rpg@xs4all.nl>
2012-08-26 19:08:17 +0200

    Add the exception library.
    Also adds the initial CMake project files and the INSTALL information.

---
+++ src/lib/exception/exception.cpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2012 by Mark de Wever <koraq@xs4all.nl>
+ * Part of the Drakken project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY.
+ *
+ * See the COPYING file for more details.
+ */
+
+#include "lib/exception/exception.hpp"
+
+namespace lib {
+
+texception::texception(const std::string& message)
+	: message_(message)
+{
+}
+
+texception::~texception() throw()
+{
+}
+
+const char*
+texception::what() const throw()
+{
+	return message_.c_str();
+}
+
+
+tpre_condition::tpre_condition(const std::string& message)
+	: texception(message)
+{
+}
+
+tpost_condition::tpost_condition(const std::string& message)
+	: texception(message)
+{
+}
+
+tunimplemented::tunimplemented(const std::string& message)
+	: texception(message)
+{
+}
+
+} // namespace lib
---
+++ src/lib/exception/exception.hpp
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2012 by Mark de Wever <koraq@xs4all.nl>
+ * Part of the Drakken project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY.
+ *
+ * See the COPYING file for more details.
+ */
+
+/**
+ * @file
+ * Contains the exceptions hierarchy for the drakken suite.
+ *
+ * Some of the base classes can't be thrown, they are only around to provide
+ * nodes in the hierarchy. These nodes use protected constructors to avoid
+ * being created directly.
+ */
+
+#ifndef LIB_EXCEPTION_EXCEPTION_HPP_INCLUDED
+#define LIB_EXCEPTION_EXCEPTION_HPP_INCLUDED
+
+#include <string>
+
+namespace lib {
+
+/**
+ * The base class for all VM exceptions.
+ *
+ * @note The exception isn't throw directly, but one of the classes inheriting
+ * this exception will.
+ */
+class texception
+	: public std::exception
+{
+protected:
+
+	texception(const std::string& message);
+
+public:
+
+	virtual ~texception() throw();
+
+	virtual const char* what() const throw();
+
+private:
+
+	std::string message_;
+};
+
+/** Thrown when a pre-condition is violated. */
+struct tpre_condition
+	: public texception
+{
+	tpre_condition(const std::string& message);
+};
+
+/** Thrown when a post-condition is violated. */
+struct tpost_condition
+	: public texception
+{
+	tpost_condition(const std::string& message);
+};
+
+/** Thrown when the wanted functionality has not yet been implemented. */
+struct tunimplemented
+	: public texception
+{
+	tunimplemented(const std::string& message);
+};
+
+} // namespace lib
+
+#endif
